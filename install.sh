955fe90e4899b5e7019f4483faaf4c9a11d3dd6f
bmaupin <bmaupin@users.noreply.github.com>
2023-11-20 12:21:34 -0500

    feat: Install to BtS
    ... and rename mod since it seems to fit better. Also add more files to NOTICE

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-GAME_BASE="${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization IV Beyond the Sword/"
+GAME_BASE="${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization IV Beyond the Sword/Beyond the Sword"
 
-mkdir -p "${GAME_BASE}/Mods/Micro Civ IV"
-cp -r src/* "${GAME_BASE}/Mods/Micro Civ IV/"
+MOD_NAME="Micro Civ 4"
+
+mkdir -p "${GAME_BASE}/Mods/${MOD_NAME}"
+cp -r src/* "${GAME_BASE}/Mods/${MOD_NAME}/"
